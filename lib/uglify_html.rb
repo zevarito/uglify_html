c2735c8ac31176ae6e2974e37ed268a402fe749b
Álvaro Gil <zevarito@gmail.com>
2009-08-10 18:36:36 -0300

    refactor the way of it is processed a little

--- lib/uglify_html.rb
+++ lib/uglify_html.rb
@@ -8,25 +8,24 @@ class UglifyHtml
   def make_ugly
     (@doc/"*").each do |e|
       case e.name
-      when 'b', 'strong'
-        if e.parent and e.parent.name == "span" and not e.previous_node and not e.next_node
-          e.parent.set_style("font-weight", "bold")
-          e.swap e.inner_html
-        else
-          e.change_tag! "span"
-          e.set_style("font-weight", "bold")
-        end
-      when 'em', 'i'
-        if e.parent and e.parent.name == "span" and not e.previous_node and not e.next_node
-          e.parent.set_style("font-style", "italic")
-          e.swap e.inner_html
-        else
-          e.change_tag! "span"
-          e.set_style("font-style", "italic")
-        end
-      end
+      when 'b', 'strong' then process(e, "font-weight", "bold")
+      when 'i', 'em'     then process(e, "font-style",  "italic")
+      end 
+
     end
 
     @doc.to_html
+  end
+
+  private
+
+  def process(e, style, value)
+    if e.parent and e.parent.name == "span" and e.parent.children.size == 1
+      e.parent.set_style(style, value)
+      e.swap e.inner_html
+    else
+      e.change_tag! "span"
+      e.set_style(style, value)
+    end
   end
 end
